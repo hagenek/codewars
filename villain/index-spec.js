9770f677ca3ed1ba0a9a0fe06cd4276b79ce2cea
Georg Ekeberg <georghagen@gmail.com>
2020-12-09 12:52:55 +0100

    solved

--- villain/index-spec.js
+++ villain/index-spec.js
@@ -1,10 +1,16 @@
 const getVillainName = require('./index.js')
 const { assert } = require('chai')
 
-
 it("should ...", () => {
   assert.equal(getVillainName(new Date("May 3")), "The Despicable Raisin")
-  assert.equal(getVillainName(new Date("April 21")), "The Trashy Pickle")
+})
+it("should ...", () => {
+
   assert.equal(getVillainName(new Date("December 17")), "The Awkward House Cat")
 })
+
+it("should ...", () => {
+  assert.equal(getVillainName(new Date("April 21")), "The Trashy Pickle")
+})
+
 
--- villain/index.js
+++ villain/index.js
@@ -1,11 +1,11 @@
-function getVillainName(birthday){
+function getVillainName(birthday) {
   const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing",
             "Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"]
 
   const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato",
               "Tomato","House Cat","Teaspoon","Laundry Basket"]
 
-
+  return "The " + m[birthday.getMonth()] + " " + d[birthday.getDate().toString().slice(-1)]
 
 }
 
